2dd3c61880d8d4850e02d4c656f33345f745b8f3
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-09-23 16:06:49 -0400

    Add role column to database

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -42,6 +43,12 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => 'string',
         ];
+    }
+    // Relationships
+    public function contacts()
+    {
+        return $this->hasMany(Contact::class);
     }
 }
