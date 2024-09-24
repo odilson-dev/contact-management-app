f31f1efc452cf2c8b8275fcbd3153fc55772c207
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-09-24 14:47:02 -0400

    Create Navbar

---
+++ resources/js/Components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="flex justify-between p-4 bg-gray-200">
+        <div class="text-lg">Navbar</div>
+
+        <div>
+            <ul class="flex gap-x-4 me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <Link class="text-dark" :href="route('myhome')">Home</Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
